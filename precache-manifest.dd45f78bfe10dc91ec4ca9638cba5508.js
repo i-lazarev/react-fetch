1c53e95a163c7f20dac6ae2013909697ad485369
i-lazarev <vanealazarev@gmail.com>
2019-12-17 15:34:43 +0100

    Updates

---
+++ precache-manifest.dd45f78bfe10dc91ec4ca9638cba5508.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0ba24df44079566fe428a193d516c674",
+    "url": "/react-fetch/index.html"
+  },
+  {
+    "revision": "04e17809bc48b4149f01",
+    "url": "/react-fetch/static/css/main.831e22c5.chunk.css"
+  },
+  {
+    "revision": "b2cd8fa03f7d5b80ad1c",
+    "url": "/react-fetch/static/js/2.843fec27.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/react-fetch/static/js/2.843fec27.chunk.js.LICENSE"
+  },
+  {
+    "revision": "04e17809bc48b4149f01",
+    "url": "/react-fetch/static/js/main.f82f1b49.chunk.js"
+  },
+  {
+    "revision": "06ff2b5847961d593048",
+    "url": "/react-fetch/static/js/runtime-main.ce02d12b.js"
+  }
+]);
